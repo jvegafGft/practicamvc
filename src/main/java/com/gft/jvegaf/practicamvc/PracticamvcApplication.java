08ad75dedbc17be84cf27d37792d3c65b93a97cf
Jose Vega <Jose.Vega@gft.com>
2022-05-20 22:46:41 +0200

    Project Boilerplate
    Signed-off-by: Jose Vega <Jose.Vega@gft.com>

---
+++ src/main/java/com/gft/jvegaf/practicamvc/PracticamvcApplication.java
@@ -0,0 +1,13 @@
+package com.gft.jvegaf.practicamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticamvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticamvcApplication.class, args);
+    }
+
+}
