57f24476421524461386636ae8e42e7e256a8869
Jose Vega <Jose.Vega@gft.com>
2022-05-20 22:47:39 +0200

    Project boilerplate

---
+++ src/test/java/com/gft/jvegaf/practicamvc/PracticamvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.jvegaf.practicamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticamvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
